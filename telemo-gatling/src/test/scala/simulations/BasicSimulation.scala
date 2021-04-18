3f0d57f43fab610f0a4296b6dff6991fcd62511c
Julio Faerman <jfaerman@gmail.com>
2021-04-18 14:05:11 +0200

    Improved simulation

--- telemo-gatling/src/test/scala/simulations/BasicSimulation.scala
+++ telemo-gatling/src/test/scala/simulations/BasicSimulation.scala
@@ -1,26 +1,36 @@
 package simulations
 
 import scala.concurrent.duration._
-
-import io.gatling.core.Predef._ // 2
+import io.gatling.core.Predef._
+import io.gatling.core.session.Expression
 import io.gatling.http.Predef._
 
 class BasicSimulation extends Simulation { // 3
+  var baseUrl = sys.env.getOrElse("GATLING_BASE_URL", "http://localhost:8080")
+  var acceptHdr = "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"
+  var doNotTrackHdr = "1"
+  var userAgentHdr = "Mozilla/5.0 (Windows NT 5.1; rv:31.0) Gecko/20100101 Firefox/31.0"
 
-  val httpProtocol = http // 4
-    .baseUrl("http://computer-database.gatling.io") // 5
-    .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8") // 6
-    .doNotTrackHeader("1")
-    .acceptLanguageHeader("en-US,en;q=0.5")
-    .acceptEncodingHeader("gzip, deflate")
-    .userAgentHeader("Mozilla/5.0 (Windows NT 5.1; rv:31.0) Gecko/20100101 Firefox/31.0")
+  val httpProtocol = http
+    .baseUrl(baseUrl)
+    .acceptHeader(acceptHdr)
+    .doNotTrackHeader(doNotTrackHdr)
+    .userAgentHeader(userAgentHdr)
 
-  val scn = scenario("BasicSimulation") // 7
-    .exec(http("request_1") // 8
-      .get("/")) // 9
-    .pause(5) // 10
+  val get_hb = http("get_hb").get("/api/hb")
+  val get_stats = http("get_stats").get("/api/hb/stats")
 
-  setUp( // 11
-    scn.inject(atOnceUsers(1)) // 12
-  ).protocols(httpProtocol) // 13
+  val scn = scenario("Telemetry Simulation")
+    .exec(get_hb)
+    .exec(get_stats)
+    .pause(5)
+
+  setUp(
+    scn.inject(
+      incrementUsersPerSec(20)
+        .times(5)
+        .eachLevelLasting(30.seconds)
+        .startingFrom(10)
+    )
+  ).protocols(httpProtocol)
 }
