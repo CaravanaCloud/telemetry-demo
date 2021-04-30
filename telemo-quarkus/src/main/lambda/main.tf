8ea6bc3b98cc214de8f2c020f26dc8aa875a6a29
Julio Faerman <jfaerman@gmail.com>
2021-04-30 17:12:22 +0200

    ifs

--- telemo-quarkus/src/main/lambda/main.tf
+++ telemo-quarkus/src/main/lambda/main.tf
@@ -65,7 +65,6 @@ resource "aws_lambda_permission" "apigw" {
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.telemo_lambda.arn
   principal     = "apigateway.amazonaws.com"
-
   source_arn = "${aws_apigatewayv2_api.api.execution_arn}/*/*"
 }
 
