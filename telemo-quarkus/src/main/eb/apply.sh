c952b0b9787e818d980f2247fcf7abce4e15a68a
Julio Faerman <jfaerman@gmail.com>
2021-04-30 16:47:30 +0200

    Improved deployment

---
+++ telemo-quarkus/src/main/eb/apply.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+terraform init -upgrade -force-copy \
+  -backend-config="bucket=$TF_VAR_bucket_name" \
+  -backend-config="key=terraform/${TF_VAR_env_name}/state" \
+  -backend-config="region=$AWS_REGION"
+
+terraform apply -auto-approve
+
+terraform output
--- telemo-quarkus/src/main/eb/apply_eb.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-terraform apply -auto-approve
-terraform output
-
